a8595d9892c6f309299a5a666f7a50622aa91f02
Justin Muskopf <jaymuskopf@gmail.com>
2018-07-23 00:44:26 -0500

    Add relocated files

---
+++ server/serverRoutine.py
@@ -0,0 +1,23 @@
+from subprocess import Popen, PIPE, STDOUT
+from sys import stdout, exit
+from MCLib import ServerHandler
+
+DEBUG_LOG = 'debug_log.txt'
+
+def main(handler):
+    handler.startServer()
+
+if __name__ == "__main__":
+ 
+    check = Popen(['netstat', '-lnp'], stdout = PIPE, stderr = PIPE)
+    out, err = check.communicate()
+    if 'java' in out and '25565' in out:
+        print("Server already running... Exiting")
+        exit(0)
+    
+    handler = ServerHandler()
+    try:
+        main(handler)
+    except KeyboardInterrupt:
+        handler.stopServer()
+        exit(0)
---
+++ server/startServer.sh
@@ -0,0 +1,3 @@
+cat /home/administrator/workspace/minecraft_server/server/startServer.sh | at now + 5 minutes
+
+/usr/bin/python /home/administrator/workspace/minecraft_server/server/serverRoutine.py
